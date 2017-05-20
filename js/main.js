a2ebe085889c30aa2c671ce60e51949c0d70df88
Odunayo Obajuluwa <odunayoobajuluwa@Odunayos-MacBook-Pro.local>
2017-05-20 14:58:16 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardOne);
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0]);
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+
+if 	(cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+	}
+}
