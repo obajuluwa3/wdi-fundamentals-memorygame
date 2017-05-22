564fb1ff032704ec5f03b0a698a3456bd861a4a7
Odunayo Obajuluwa <odunayoobajuluwa@Odunayos-MacBook-Pro.local>
2017-05-22 00:15:12 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -22,22 +22,35 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+
 var checkForMatch = function() {
+//	this.setAttribute('src', cards[cardId].cardImage);
 	if 	(cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
-}
+};
 
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
-}
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+    checkForMatch();
+    cardsInPlay = [];
+  }
+};
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement)
+	}
+};
+
+createBoard();
 
